c7c222a2b729ad5022e0e3be2bf5be19bd93bd70
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-08-22 01:10:20 +0530

    add style

--- pages/index.js
+++ pages/index.js
@@ -48,48 +48,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <div className={`${cookieConsent != null ? "hidden" : "flex"}`}>
+      <div style={{ display: cookieConsent != null ? "none" : "flex", 
+              margin: "2.5rem auto", 
+              maxWidth: "max-content", 
+              position: "fixed", 
+              bottom: 0, 
+              left: 0, 
+              right: 0, 
+              padding: "0.75rem 1rem", 
+              justifyContent: "space-between", 
+              alignItems: "center", 
+              flexDirection: "column", 
+              gap: "1rem", 
+              backgroundColor: "#4B5563", // bg-gray-700
+              borderRadius: "0.5rem", 
+              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)" }}>
 
         <div className='text-center'>
           <Link href="/info/cookies">

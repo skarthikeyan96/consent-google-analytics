19e69abbd07aa89123ba0f0bbc5538b238d68242
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-08-22 00:52:22 +0530

    iniital commit

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+// _document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-B71N5R0MH5`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B71N5R0MH5', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,45 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { getLocalStorage, setLocalStorage } from '../lib/storage_helper'
+
+// declare global {
+//   interface Window {
+//       gtag: (...args: any[]) => void;
+//   }
+// }
+
 
 export default function Home() {
+  const [cookieConsent, setCookieConsent] = useState(false);
+
+  useEffect(() => {
+    const storedCookieConsent = getLocalStorage("cookie_consent", null);
+    console.log("Stored Cookie Consent:", storedCookieConsent);
+    setCookieConsent(storedCookieConsent);
+  }, [setCookieConsent]);
+
+  useEffect(() => {
+    // if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      const newValue = cookieConsent ? 'granted' : 'denied';
+      console.log("Setting cookie consent to:", newValue);
+      
+      
+     window.gtag('consent', 'update', {
+        'analytics_storage': newValue,
+      });
+
+      setLocalStorage("cookie_consent", cookieConsent);
+      
+      // For Testing
+      console.log("Cookie Consent (final):", cookieConsent);
+    // } else {
+    //   console.log("gtag is not available or window is undefined");
+    // }
+  }, [cookieConsent]);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -51,6 +88,31 @@ export default function Home() {
           </a>
         </div>
       </main>
+
+      <div className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+                        fixed bottom-0 left-0 right-0 
+                        flex px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
+                         bg-gray-700 rounded-lg shadow`}>
+
+        <div className='text-center'>
+          <Link href="/info/cookies">
+            <p>We use <span className='font-bold text-sky-400'>cookies</span> on our site.</p>
+          </Link>
+        </div>
+
+        <div className='flex gap-2'>
+          <button 
+            className='px-5 py-2 text-gray-300 rounded-md border-gray-900' 
+            onClick={() => setCookieConsent(false)}>
+            Decline
+          </button>
+          <button 
+            className='bg-gray-900 px-5 py-2 text-white rounded-lg' 
+            onClick={() => setCookieConsent(true)}>
+            Allow Cookies
+          </button>
+        </div>   
+      </div>
 
       <footer className={styles.footer}>
         <a

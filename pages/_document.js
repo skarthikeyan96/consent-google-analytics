ec3c77bd998d67a3d3b62b368fb4dc0718276a56
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-08-22 01:28:11 +0530

    fix edge cages

--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,48 @@
+// // _document.js
+// import { Html, Head, Main, NextScript } from 'next/document'
+
+// export default function Document() {
+
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         {/* Global Site Tag (gtag.js) - Google Analytics */}
+//         <script
+//           async
+//           src={`https://www.googletagmanager.com/gtag/js?id=G-B71N5R0MH5`}
+//         />
+//         <script
+//           dangerouslySetInnerHTML={{
+//             __html: `
+//               window.dataLayer = window.dataLayer || [];
+//               function gtag(){dataLayer.push(arguments);}
+//               gtag('js', new Date());
+//               gtag('consent', 'default', {
+//                 'ad_storage': 'denied',
+//                 'ad_user_data': 'denied',
+//                 'ad_personalization': 'denied',
+//                 'analytics_storage': 'denied'
+//               });
+//               gtag('config', 'G-B71N5R0MH5', {
+//                 page_path: window.location.pathname,
+//               });
+//             `,
+//           }}
+//         />
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   )
+// }
+
+// _document.js
 // _document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-
   return (
     <Html lang="en">
       <Head>
@@ -19,8 +59,6 @@ export default function Document() {
               gtag('js', new Date());
               gtag('consent', 'default', {
                 'ad_storage': 'denied',
-                'ad_user_data': 'denied',
-                'ad_personalization': 'denied',
                 'analytics_storage': 'denied'
               });
               gtag('config', 'G-B71N5R0MH5', {
@@ -37,3 +75,4 @@ export default function Document() {
     </Html>
   )
 }
+

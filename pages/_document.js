08988fc063fb8b504b694ef22644e6b515d24c4b
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-08-22 10:06:13 +0530

    non working demo

--- pages/_document.js
+++ pages/_document.js
@@ -1,44 +1,3 @@
-// // _document.js
-// import { Html, Head, Main, NextScript } from 'next/document'
-
-// export default function Document() {
-
-//   return (
-//     <Html lang="en">
-//       <Head>
-//         {/* Global Site Tag (gtag.js) - Google Analytics */}
-//         <script
-//           async
-//           src={`https://www.googletagmanager.com/gtag/js?id=G-B71N5R0MH5`}
-//         />
-//         <script
-//           dangerouslySetInnerHTML={{
-//             __html: `
-//               window.dataLayer = window.dataLayer || [];
-//               function gtag(){dataLayer.push(arguments);}
-//               gtag('js', new Date());
-//               gtag('consent', 'default', {
-//                 'ad_storage': 'denied',
-//                 'ad_user_data': 'denied',
-//                 'ad_personalization': 'denied',
-//                 'analytics_storage': 'denied'
-//               });
-//               gtag('config', 'G-B71N5R0MH5', {
-//                 page_path: window.location.pathname,
-//               });
-//             `,
-//           }}
-//         />
-//       </Head>
-//       <body>
-//         <Main />
-//         <NextScript />
-//       </body>
-//     </Html>
-//   )
-// }
-
-// _document.js
 // _document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
@@ -47,21 +6,29 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Set the default consent state without loading the GA script */}
-        {/* <script
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-B71N5R0MH5`}
+        />
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
               gtag('consent', 'default', {
                 'ad_storage': 'denied',
-                'analytics_storage': 'denied',
-                'functionality_storage': 'denied',
-                'personalization_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied'
+              });
+              gtag('config', 'G-B71N5R0MH5', {
+                page_path: window.location.pathname,
               });
             `,
           }}
-        /> */}
+        />
       </Head>
       <body>
         <Main />
